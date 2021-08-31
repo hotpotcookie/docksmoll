b3fda3d3ec521463f5e2d0d0009d4a640f78c825
aster-max <icatmuhammad2@gmail.com>
2021-08-31 11:29:54 +0700

    passed on ubuntu 20.04

--- source/run-auto-installer.sh
+++ source/run-auto-installer.sh
@@ -9,7 +9,7 @@ ln -snf /usr/share/zoneinfo/$CONTAINER_TIMEZONE /etc/localtime && echo $CONTAINE
 #----------
 apt-get update -y & wait; echo -e "----------------------------------------- ${GREEN}(installing dependencies)${ENDCOLOR}"
 apt install build-essential apt-transport-https ca-certificates curl software-properties-common -y & wait; echo -e "----------------------------------------- ${GREEN}(adding gpg key)${ENDCOLOR}"
-\curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - & wait; echo -e "----------------------------------------- ${GREEN}(adding docker to apt lists)${ENDCOLOR}"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - & wait; echo -e "----------------------------------------- ${GREEN}(adding docker to apt lists)${ENDCOLOR}"
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" & wait; echo -e "----------------------------------------- ${GREEN}(updating apt)${ENDCOLOR}"
 apt-get update -y & wait; echo -e "----------------------------------------- ${GREEN}(updating apt cache)${ENDCOLOR}"
 apt-cache policy docker-ce & wait; echo -e "----------------------------------------- ${GREEN}(installing docker)${ENDCOLOR}"
