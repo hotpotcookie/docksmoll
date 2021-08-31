0ffd411ccecf9c7776d50a7730df1567cb70dc86
aster-max <icatmuhammad2@gmail.com>
2021-09-01 00:40:49 +0700

    adding actions and verifications at deletation

--- source/run-menu.sh
+++ source/run-menu.sh
@@ -2,6 +2,7 @@
 #----------
 GREEN="\e[32m"
 YELLOW="\e[33m"
+BLUE="\e[36m"
 ENDCOLOR="\e[0m"
 #----------
 main() {
@@ -10,34 +11,38 @@ main() {
 		docker image ls | awk 'NR!=1' | tr -s ' ' '@' > tmp/image.lst & wait
 		docker container ls -a | awk 'NR!=1' | tr -s ' ' '@' > tmp/container.lst & wait
 		curdate=$(date +'%D %T %p')
-
-		echo -e "${GREEN}docksmoll 1.1.1${ENDCOLOR}                  $curdate"
+		echo -e "${YELLOW}docksmoll 1.1.1${ENDCOLOR}                  $curdate"
 		echo -e "-----------------------------------------------------"
-		# docker image	
+		body_img=$(sudo docker images -a --format "table {{.Repository}}:{{.Tag}}\t| {{.ID}} | {{.Size}}" | awk 'NR!=1')
+		body_ctr=$(sudo docker container ls -a --format "table {{.Names}} ({{.Image}})\t| {{.ID}} | {{.Status}}" | awk 'NR!=1')
+		# docker image -a
 		# docker ps
 		if [[ ! -s tmp/image.lst ]]; then
 			echo "NO IMAGES HAVE BEEN LOADED"
 		else
-			cat tmp/image.lst		
-		fi
+			echo -e "${GREEN}AVAILABLE IMAGES${ENDCOLOR}\n-----------------------------------------------------"
+			echo -e "$body_img"; echo "-----------------------------------------------------"; fi
 		if [[ ! -s tmp/container.lst ]]; then
 			echo "NO CONTAINERS HAVE BEEN CREATED"
 		else
-			cat tmp/container.lst		
-		fi
+			echo -e "${GREEN}CREATED CONTAINERS${ENDCOLOR}\n-----------------------------------------------------"
+			echo -e "$body_ctr"; echo "-----------------------------------------------------"; fi
 
+		echo -e "====================================================="
+		echo -e "${BLUE}[1]${ENDCOLOR} SEARCH IMAGE | ${BLUE}[4]${ENDCOLOR} CREATE CONTAINER | ${YELLOW}[7]${ENDCOLOR} START"
+		echo -e "${BLUE}[2]${ENDCOLOR} PULL IMAGE   | ${BLUE}[5]${ENDCOLOR} RENAME CONTAINER | ${YELLOW}[8]${ENDCOLOR} STOP"
+		echo -e "${BLUE}[3]${ENDCOLOR} DROP IMAGE   | ${BLUE}[6]${ENDCOLOR} DROP CONTAINER   | ${YELLOW}[9]${ENDCOLOR} RESTART"
 		echo -e "-----------------------------------------------------"
-		echo -e "[1] SEARCH IMAGE | [4] CREATE CONTAINER | [7] START"
-		echo -e "[2] PULL IMAGE   | [5] RENAME CONTAINER | [8] STOP"
-		echo -e "[3] DROP IMAGE   | [6] DROP CONTAINER   | [9] RESTART"
+		echo -e "${YELLOW}[S]${ENDCOLOR} JOIN SHELL   | ${BLUE}[C]${ENDCOLOR} CLEAR SCREEN     | ${BLUE}[E]${ENDCOLOR} EXIT   "
 		echo -e "-----------------------------------------------------"
 		while :; do
 			echo -en "${YELLOW}>>${ENDCOLOR}"
 			read -p " " opt
-			case $opt in 
+			case $opt in
 				1) search_img;;	4) create_ctr;;	7) start_ctr;;
 				2) pull_img;;	5) rename_ctr;;	8) stop_ctr;;
 				3) drop_img;;	6) drop_ctr;;	9) restart_ctr;;
+				"S") join_shell;; "C") break;; "E") exit 0;;
 			esac
 		done
 	done
@@ -45,15 +50,71 @@ main() {
 
 search_img() {
 	echo "--"
-	read -p ":: ENTER IMAGE KEYWORD: " image
-	docker search $image
-	echo "--"	
+	read -p ":: ENTER IMAGE NAME: " image
+	header=$(docker search --format "table {{.Name}} ({{.StarCount}})\t| {{.IsAutomated}}\t| {{.IsOfficial}}" $image | awk 'NR==1')
+	body=$(docker search --format "table {{.Name}} ({{.StarCount}})\t| {{.IsAutomated}}\t| {{.IsOfficial}}" $image | awk 'NR!=1')
+	header_len=$(echo "$header" | wc -c); header_len=$((--header_len))
+	for i in $(seq 1 $header_len); do echo -n "-" ;done; echo ""
+	echo -e "${GREEN}$header${ENDCOLOR}"
+	for i in $(seq 1 $header_len); do echo -n "-" ;done; echo ""
+	echo "$body"
+	for i in $(seq 1 $header_len); do echo -n "-" ;done; echo ""
+	echo " "
 }
 pull_img() {
 	echo "--"
-	read -p ":: ENTER IMAGE KEYWORD: " image
+	read -p ":: ENTER IMAGE NAME: " image
+	echo "--"
 	docker pull $image
-	echo "--"		
+	echo " "
+}
+drop_img() {
+	echo "--"
+	read -p ":: ENTER IMAGE NAME: " image
+	read -p ":: RE-ENTER IMAGE NAME: " image2
+	echo "--"
+	check_ref=$(docker container ls -a | grep "$image")
+	if [[ "$image" == "$image2" ]]; then
+		if [[ "$image" && "$image" != "\n" && "$image" == " " ]]; then
+			echo "$check_ref"
+			echo "image $image have reference(s) to existing container"
+			read -p "force remove image ? (y): " opt
+			if [[ "$opt" == "y" || "$opt" == "Y" ]]; then
+				docker rmi -f $image
+				docker image prune -f
+			fi
+		else
+			docker rmi $image
+			docker image prune -f
+		fi
+	fi
+	echo " "
+}
+create_ctr() {
+	echo "--"
+	read -p ":: ENTER IMAGE NAME: " image
+	read -p ":: ENTER CONTAINER NAME: " container
+	echo "--"
+	echo -n ":: container hash: "; docker create --name "$container" $image
+	echo " "
+}
+rename_ctr() {
+	echo "--"
+	read -p ":: ENTER CONTAINER NAME: " container
+	read -p ":: ENTER NEW CONTAINER NAME: " container2
+	echo "--"
+	echo ":: remaed container: $container > $container2"; docker rename "$container" "$container2"
+	echo " "
+}
+drop_ctr() {
+	echo "--"
+	read -p ":: ENTER CONTAINER NAME: " container
+	read -p ":: RE-ENTER CONTAINER NAME: " container2
+	echo "--"
+	if [[ "$container" == "$container2" ]]; then
+		echo -n ":: deleted container: "; docker rm "$container"
+	fi
+	echo " "
 }
 
 ## MAIN METHOD
@@ -68,3 +129,12 @@ main
 # - docker start/stop/restart $container_id    ## manage container service
 # - docker exec -it $container_name /bin/bash  ## getting to container's tty
 # - docker info                                ## get additional info
+# ----------------------------------------------------------------------
+# ~ ubuntu (12696)
+#	docker search $image | awk 'NR!=1' | tr -s ' ' '@' > tmp/search_img.lst
+#	while read -r line; do
+#		range_deli=$(echo "$line" | tr -cd '@' | wc -c)
+#		range_deli=$((--range_deli))
+#		img_name=$(echo $line | cut -d '@' -f 1)
+#		echo "$range_deli $img_name"
+#	done < "tmp/search_img.lst"
